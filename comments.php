062dbabf941f5f22c66073fe76cc32b4ab763645
Armin <arminmesic@hotmail.de>
2014-08-08 10:49:22 +0200

    Comment form changes

--- comments.php
+++ comments.php
@@ -64,6 +64,17 @@ if ( post_password_required() ) {
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'cartan' ); ?></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<?php $comments_args = array(
+	        // change the title of send button 
+	        'label_submit'=>'Send',
+	        // change the title of the reply section
+	        'title_reply'=>'Write a Reply or Comment',
+	        // remove "Text or HTML to be displayed after the set of comment fields"
+	        'comment_notes_after' => '',
+	        // redefine your own textarea (the comment body)
+	        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+	);
+	
+	comment_form(); ?>
 
 </div><!-- #comments -->
