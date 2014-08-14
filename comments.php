776a2cede68c7db6dcae3bc322a0310debb37dcc
Armin <arminmesic@hotmail.de>
2014-08-14 16:36:26 +0200

    Change comment form

--- comments.php
+++ comments.php
@@ -23,7 +23,7 @@ if ( post_password_required() ) {
 	<?php // You can start editing here -- including this comment! ?>
 
 	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
+		<h2 class="comments-title gamma">
 			<?php
 				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'cartan' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
@@ -64,15 +64,41 @@ if ( post_password_required() ) {
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'cartan' ); ?></p>
 	<?php endif; ?>
 
+	<?php $commenter = wp_get_current_commenter();
+$req = get_option( 'require_name_email' );
+$aria_req = ( $req ? " aria-required='true'" : '' ); ?>
 	<?php $comments_args = array(
 	        // change the title of send button 
-	        'label_submit'=>'Send',
 	        // change the title of the reply section
 	        'title_reply'=>'Write a Reply or Comment',
-	        // remove "Text or HTML to be displayed after the set of comment fields"
-	        'comment_notes_after' => '',
-	        // redefine your own textarea (the comment body)
-	        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+	        // adds custom button default button dispaly:none and remove "Text or HTML to be displayed after the set of comment fields and"
+	        'comment_notes_after' => '<button type="submit" id="submit-new" class="button button-submit"><span>'.__('Post Comment').'</span></button>',
+
+					'fields' => apply_filters( 'comment_form_default_fields', array(
+						//opens .row
+  				  'author' =>
+  				    '<div class="row"> <div class="large-6 medium-6 columns"> <p class="comment-form-author">' .
+  				    '<label for="author" class="micro">' . __( 'Name', 'domainreference' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' .
+  				    
+  				    '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+  				    '" size="30"' . $aria_req . ' /></p> </div>',
+				
+  				  'email' =>
+  				    '<div class="large-6 medium-6 columns"> <p class="comment-form-email"><label for="email" class="micro">' . __( 'Email', 'domainreference' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' .
+  				    '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+  				    '" size="30"' . $aria_req . ' /></p> </div>',
+				
+  				  'url' =>
+  				    '<div class="columns"> <p class="comment-form-url"><label for="url" class="micro">' .
+  				    __( 'Website', 'domainreference' ) . '</label>' .
+  				    '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+  				    '" size="30" /></p> </div>'
+  				  )
+  				),
+
+					// closes .row redefine your own textarea (the comment body)
+	        'comment_field' => '<div class="columns"> <p class="comment-form-comment"><label for="comment" class="micro">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" aria-required="true"></textarea></p> </div> </div>',
+
 	);
 	
 	comment_form( $comments_args ); ?>
