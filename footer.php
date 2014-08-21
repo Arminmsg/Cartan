c7607d881f1c68a4c5903306c3ca7cd6d6d8604d
Armin <arminmesic@hotmail.de>
2014-08-21 17:09:32 +0200

    Fix Searchpage Layout and Style Footer

--- footer.php
+++ footer.php
@@ -10,12 +10,12 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer micro" role="contentinfo">
     <div class="row">
      <div class="site-info columns">
       <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'cartan' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'cartan' ), 'WordPress' ); ?></a>
       <span class="sep"> | </span>
-      <?php printf( __( 'Theme: %1$s by %2$s.', 'cartan' ), 'Cartan', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+      <?php printf( __( 'Theme: %1$s by %2$s.', 'cartan' ), 'Cartan', '<a href="http://arminmesic.com/" rel="designer">Armin Mesic</a>' ); ?>
      </div><!-- .site-info --> 
     </div>
 	</footer><!-- #colophon -->
